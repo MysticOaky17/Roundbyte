8c1a4968a238c80f348ca9750d69e1798d7952d8
MysticOaky17 <239385805+MysticOaky17@users.noreply.github.com>
2025-12-09 21:01:36 +0100

    Add tRPC

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,4 +5,29 @@ import type { AppProps } from "next/app";
 function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
 }
-export default MyApp;
+
+import { withTRPC } from "@trpc/next";
+import type { AppRouter } from "@/backend/router";
+
+export default withTRPC<AppRouter>({
+  config(config) {
+    /**
+     * If you want to use SSR, you need to use the server's full URL
+     * @see https://trpc.io/docs/ssr
+     */
+    const url = process.env.VERCEL_URL
+      ? `https://${process.env.VERCEL_URL}/api/trpc`
+      : "http://localhost:3000/api/trpc";
+    return {
+      url,
+      /**
+       * @see https://tanstack.com/query/v3/docs/react/reference/QueryClient
+       */
+      // queryClientConfig: { defaultOptions: { queries: { staleTime: 60 } } },
+    };
+  },
+  /**
+   * @see https://trpc.io/docs/ssr
+   */
+  ssr: true,
+})(MyApp);
